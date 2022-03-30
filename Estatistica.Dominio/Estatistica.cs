979d933cdeb3c613aeb9b3a4d3902f781749b5b2
gomesluiz <gomes.luiz@gmail.com>
2022-03-30 19:26:18 -0300

    inclui testes de unidade

--- Estatistica.Dominio/Estatistica.cs
+++ Estatistica.Dominio/Estatistica.cs
@@ -2,6 +2,24 @@
 {
     public class Estatistica
     {
+        public static double Media(double[] valores, double minimo, double maximo)
+        {
+            double soma = 0.0, media = 0.0;
+            int i = 0, validas = 0;
 
+            while((i < 100) && (valores[i] != -999))
+            {
+                if ((valores[i] >= minimo) && (valores[i] <= maximo))
+                {
+                    soma += valores[i];
+                    validas++;
+                }
+                i++;
+            }
+            if (validas > 0)
+                media = soma / validas;
+
+            return media;
+        }
     }
 }
