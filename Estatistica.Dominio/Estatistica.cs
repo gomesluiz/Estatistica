739d625aa3f776578ffc1384bd1f027f255c9352
gomesluiz <gomes.luiz@gmail.com>
2022-05-20 22:58:18 -0300

    test: teste de mensagem de commit
    dados n numeros.....
    
    Resolves: #10

--- Estatistica.Dominio/Estatistica.cs
+++ Estatistica.Dominio/Estatistica.cs
@@ -2,6 +2,35 @@
 {
     public class Estatistica
     {
+        /// <summary>
+        /// O método <c>Media</c> calcula a média de 100 números ou menos,
+        /// situados entre os valores mínimo e máximo. Baseado no livro de
+        /// Engenharia de Software do autor Roger Pressman.
+        /// </summary>
+        /// <param name="valores">Array de números</param>
+        /// <param name="minino">Limite inferior para o cálculo da média</param>
+        /// <param name="maximo">Límite superior para o cálculo da média</param>
+        /// <returns>Media</returns>
+        public static double Media(double[] valores,
+                                   double minimo,
+                                   double maximo)
+        {
+            double soma = 0.0, media = 0.0;
+            int i = 0, validas = 0;
 
+            while ((i < 100) && (valores[i] != -999))
+            {
+                if ((valores[i] >= minimo) && (valores[i] <= maximo))
+                {
+                    soma += valores[i];
+                    validas++;
+                }
+                i++;
+            }
+            if (validas > 0)
+                media = soma / validas;
+
+            return media;
+        }
     }
 }
--- Estatistica.TesteDeUnidade/Estatistica_Media.cs
+++ Estatistica.TesteDeUnidade/Estatistica_Media.cs
@@ -1,18 +1,24 @@
 using NUnit.Framework;
+using Estatistica.Dominio;
 
 namespace Estatistica.TesteDeUnidade
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void Media_Testa_Caso_1()
         {
-            Assert.Pass();
+            // Arranjo
+            double[] valores = { 10.0, 5.0, 15.0, -999.0 };
+            double min = 0.0, Max = 50.0;
+            double esperado = (valores[0] + valores[1] + valores[2]) / 2;
+            double tolerancia = 0.01;
+
+            // Ação
+            double retornado = Dominio.Estatistica.Media(valores, min, Max);
+            
+            // Asserção
+            Assert.AreEqual(esperado, retornado, tolerancia);
         }
     }
 }
